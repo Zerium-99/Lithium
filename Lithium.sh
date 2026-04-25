927c0b5256ebf446e5ae6c5feee2c0a882b82f05
Zerium <acid9890@proton.me>
2026-04-25 17:29:24 +0200

    Update Lithium.sh

--- Lithium.sh
+++ Lithium.sh
@@ -60,14 +60,13 @@ read option # Reads the user input.
 # Installs ngrok in the machine.
 
 if [ "$option" = "1" ]; then 
+
     echo "[+] Installing Ngrok.."
     
-    curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc | \
-    sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && \
-    echo "deb https://ngrok-agent.s3.amazonaws.com bookworm main" | \
-    sudo tee /etc/apt/sources.list.d/ngrok.list && \
-    sudo apt update && \
-    sudo apt install ngrok
+    wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
+    
+    sudo tar xvzf ./ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
+    
     sudo ./Lithium.sh
 
 # Adds the Ngrok authtoken.
